8fcd6d161cd01ce5abc8c83a456e5f509bbc569c
Ivan Mankovich <mankovich.ivan@gmail.com>
2020-06-22 00:24:39 +0300

    Updates

---
+++ precache-manifest.4199a9844254e228d0c7d4ee8c6a164c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad450225a64d76edc8a7348ff9cae3a",
+    "url": "/apod/index.html"
+  },
+  {
+    "revision": "95dd570c14500664bfb6",
+    "url": "/apod/static/css/main.b157fae9.chunk.css"
+  },
+  {
+    "revision": "aebc3a210dbc00507dd6",
+    "url": "/apod/static/js/2.7f154bb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apod/static/js/2.7f154bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95dd570c14500664bfb6",
+    "url": "/apod/static/js/main.a2ee3b7a.chunk.js"
+  },
+  {
+    "revision": "910c3089301591d5e497",
+    "url": "/apod/static/js/runtime-main.9edeaabe.js"
+  }
+]);
